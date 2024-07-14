06baccdfd473163ef2baf39081ab02d8ec85f261
Nifraz Navahz <nifraz@live.com>
2024-07-14 18:21:43 +0530

    change properties to nullable, support filter null values

--- SmartSchool.Schema/Types/StudentType.cs
+++ SmartSchool.Schema/Types/StudentType.cs
@@ -3,11 +3,11 @@
     public class StudentType
     {
         [IsProjected(true)]
-        public long Id { get; set; }
-        public string StudentId { get; set; }
-        public string FullName { get; set; }
-        public string NickName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public long? Id { get; set; }
+        public string? StudentId { get; set; }
+        public string? FullName { get; set; }
+        public string? NickName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public string? MobileNo { get; set; }
     }
 }
